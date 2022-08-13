94ae576c636c5680a6d2e1a0fcae8c497765919f
leopard930418 <leopard930418@gmail.com>
2022-08-13 22:43:17 +0900

    lamboozled meter

--- components/base/Meter.js
+++ components/base/Meter.js
@@ -9,11 +9,13 @@ export default function MyImage({
   console.log("meterwidht",percent);
   return (
     <>
-    
-    <div className={`fixed h-[27px] bg-cover bg-no-repeat bg-red-700 -translate-y-20 translate-x-20`} style={{width:percent+'px'}}>
+    <div className={`fixed h-[30px] w-[280px] bg-white -translate-y-20 translate-x-20`} style={{width:percent+'px'}}>
 
     </div>
-    <div className="fixed bg-[url('/images/MarkMeter.png')] w-[280px] h-[27px] bg-cover bg-no-repeat -translate-y-20 translate-x-20">
+    <div className={`fixed h-[24px]  bg-[#c7d647] -translate-y-20 translate-x-20`} style={{width:percent+'px'}}>
+
+    </div>
+    <div className="fixed bg-[url('/images/MarkMeter.png')] w-[280px] h-[28px] bg-cover bg-no-repeat -translate-y-[5.1rem] translate-x-20">
 
     </div>
     </>
