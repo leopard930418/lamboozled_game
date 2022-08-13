0a7fb91d9973554d626e1025caeb484e9f5aeab8
leopard930418 <leopard930418@gmail.com>
2022-08-13 22:52:12 +0900

    fix meter

--- components/base/Meter.js
+++ components/base/Meter.js
@@ -4,7 +4,16 @@ export default function MyImage({
   point = 50,
  
  }) {
-
+  let color = '#000000';
+  if(point<26){
+    color='#db5f10';
+  }else if(point < 50){
+    color = '#e7b375';
+  }else if(point < 76){
+    color = '#d6d681';
+  }else{
+    color = '#93d58f';
+  }
   const [percent, setPercent] = useState(Math.ceil(280/100*point));
   console.log("meterwidht",percent);
   return (
@@ -12,7 +21,7 @@ export default function MyImage({
     <div className={`fixed h-[30px] w-[280px] bg-white -translate-y-20 translate-x-20`} style={{width:percent+'px'}}>
 
     </div>
-    <div className={`fixed h-[24px]  bg-[#c7d647] -translate-y-20 translate-x-20`} style={{width:percent+'px'}}>
+    <div className={`fixed h-[24px]  -translate-y-20 translate-x-20`} style={{width:percent+'px', backgroundColor:color}}>
 
     </div>
     <div className="fixed bg-[url('/images/MarkMeter.png')] w-[280px] h-[28px] bg-cover bg-no-repeat -translate-y-[5.1rem] translate-x-20">
