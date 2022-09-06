94c3fa221a85f5d407b0ca31c389f8bf686d8346
leopard930418 <leopard930418@gmail.com>
2022-09-06 11:58:31 +0100

    concern

--- components/base/MyTimer.js
+++ components/base/MyTimer.js
@@ -1,11 +1,12 @@
 //Alex created
 import React from "react";
 const VerticalProgress = ({ value = 50 }) => {
-
   return (
     <div className="relative h-[638px] w-8 bg-black bg-opacity-25">
       <div
-        className={`absolute ${value>50?"bg-black":"bg-[#f95555]"} bottom-0 left-0 w-full`}
+        className={`absolute ${
+          value > 50 ? "bg-black" : "bg-[#f95555]"
+        } bottom-0 left-0 w-full`}
         style={{ height: `${100 - value}%` }}
       ></div>
     </div>
@@ -19,7 +20,6 @@ export default function MyTimer() {
       counter > 0 && setInterval(() => setCounter(counter - 1), 3000);
     return () => clearInterval(timer);
   }, [counter]);
-  // console.log("counter", counter);
   return (
     <>
       <VerticalProgress value={counter} />
